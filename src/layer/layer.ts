8878afb139edf837de0d452c7239dc0db21ab86f
Hearty Oh <heartyoh@hatiolab.com>
2018-07-29 04:33:32 +0800

    dom-element renderer 적용

--- src/layer/layer.ts
+++ src/layer/layer.ts
@@ -5,16 +5,16 @@
 import { EventSource } from '../event'
 import { RootContainer } from '../component'
 import { Scene } from '../scene'
-import * as THREE from 'three'
 
-export default class Layer extends EventSource {
+export default abstract class Layer extends EventSource {
 
   protected owner: Scene
   protected _rootContainer: RootContainer
   protected _target: HTMLElement
   protected element: HTMLElement
 
-  private draw_reserved: boolean = false
+  private _draw_reserved: boolean = false
+  private _resizeTimeout
 
   constructor(owner: Scene) {
     super()
@@ -34,15 +34,15 @@ export default class Layer extends EventSource {
   }
 
   private throttle_render() {
-    if (!this.draw_reserved) {
+    if (!this._draw_reserved) {
       requestAnimationFrame(() => {
-        this.draw_reserved = false;
+        this._draw_reserved = false;
 
         this.trigger('redraw');
         this.render();
       })
     }
-    this.draw_reserved = true;
+    this._draw_reserved = true;
   }
 
   get rootContainer() {
@@ -60,47 +60,60 @@ export default class Layer extends EventSource {
       this._rootContainer = rootContainer
 
       this._rootContainer.on('render', () => {
-        this.render()
+        this.invalidate()
       })
 
       this.invalidate()
     }
   }
 
-  fitSize(element) {
+  onresize(width, height) { }
+
+  /**
+   * 부모 엘리먼트의 크기가 변화했을 때, 호출한다.
+   * 부모 엘리먼트의 크기를 1초 간격으로 쫓아가며 자신의 크기를 변화시킨다.
+   * 자신의 크기를 변화시키면서, onresize 콜백으로 변화에 따른 부수적인 작업을 할 수 있도록 한다.
+   * 1초 동안 부모 엘리먼트의 크기변화가 없으면, resize를 멈춘다.
+   */
+  resize() {
+    if (this._resizeTimeout !== undefined) {
+      clearTimeout(this._resizeTimeout)
+      delete this._resizeTimeout
+    }
+
     if (!this.target)
       return
 
-    requestAnimationFrame(() => {
+    var oldwidth
+    var oldheight
 
-      let style = getComputedStyle(this.target)
+    var checker = () => {
 
-      let width = style ? parseFloat(style.getPropertyValue('width')) : this.target.offsetWidth
-      let height = style ? parseFloat(style.getPropertyValue('height')) : this.target.offsetHeight
+      let width = this.target.offsetWidth
+      let height = this.target.offsetHeight
 
-      element.setAttribute('width', width);
-      element.setAttribute('height', height);
+      if (oldwidth == width && oldheight == height) {
+        return
+      }
 
-      element.style.width = width + 'px';
-      element.style.height = height + 'px';
+      this.element.style.width = width + 'px'
+      this.element.style.height = height + 'px'
 
+      this.onresize && this.onresize(width, height)
       this.invalidate()
-    })
+
+      oldwidth = width
+      oldheight = height
+
+      // 최종 마무리는 확실하게
+      this._resizeTimeout = setTimeout(checker, 500)
+    }
+
+    // 최초 반응은 빠르게.
+    requestAnimationFrame(checker)
   }
 
-  resize() {
-    this.fitSize(this.element)
-  }
-
-  protected createElement(): HTMLElement {
-    var element = document.createElement('canvas');
-    element.width = this.rootContainer.width;
-    element.height = this.rootContainer.height;
-
-    element.style.position = 'absolute';
-
-    return element;
-  }
+  protected abstract buildOverlays()
 
   get target() {
 
@@ -108,113 +121,41 @@ export default class Layer extends EventSource {
   }
 
   set target(target) {
-    if (this._target && this.element)
+    if (this._target && this.element) {
       this._target.removeChild(this.element);
+    }
 
     this._target = target;
 
     if (!target)
       return;
 
-    if (!this.element)
-      this.element = this.createElement();
+    if (!this.element) {
+      this.element = document.createElement('div')
+      this.element.style.position = 'absolute'
 
-    /*
-     * 캔바스의 크기 결정 순서
-     * 1. model의 설정에 크기 정보가 있다면, 그에 따라 상위 엘리먼트의 크기를 조정한다.
-     * 2. 부모 엘리먼트의 크기에 따라 캔바스의 폭과 높이를 맞춘다.
-     * 3. 캔바스의 스타일은 무조건, 부모 엘리먼트의 100%로 한다.
-     */
-
-    this.resize()
+      this.buildOverlays()
+    }
 
     target.appendChild(this.element)
 
+    this.resize()
+
     this.ready()
-
-    this.render()
   }
 
-  get canvas(): HTMLCanvasElement {
-    return this.element as HTMLCanvasElement
-  }
+  protected abstract render(context?)
 
-  // get selected() {
+  protected prerender(context?) { }
 
-  //   return this.root.selected
-  // }
-
-  // set selected(sels) {
-
-  //   this.root.selected = sels
-  // }
-
-  // get focused() {
-
-  //   return this.root.focused
-  // }
-
-  // set focused(container) {
-
-  //   this.root.focused = container
-  // }
-
-  getContext() {
-    return (this.element as HTMLCanvasElement).getContext('2d');
-  }
-
-  /* 레어어의 draw는 외부에서 context를 제공하지 않으면, 자신의 캔바스의 컨텍스트를 이용해서 그린다. */
-  render(context?) {
-    // if (!this.element)
-    //   return;
-
-    // this.renderer.render(this.rootContainer.object3D, this.camera);
-
-    // context = context || this.getContext();
-    // if (!context)
-    //   return;
-
-    // this.rootContainer.render(context)
-  }
-
-  prerender(context) {
-
-    var {
-      translate,
-      scale = {
-        x: 1,
-        y: 1
-      },
-      rotation
-    } = this.rootContainer.state;
-
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-
-    translate && context.translate(translate.x, translate.y);
-
-    context.scale(scale.x, scale.y);
-    rotation && context.rotate(rotation);
-  }
-
-  get rotatePoint() {
-
-    // 레이어의 중심은 원점이다.
-    return {
-      x: 0,
-      y: 0
-    }
-  }
-
-  /* contains는 보통 자식 클래스에서 오버라이드 해야한다. */
-  contains(x, y) {
-    return false;
-  }
-
-  invalidate() {
-    /*
-     * throttle 로직으로 호출을 최소화
-     * 빈번히 반복되는 invalidate()에 대해 비효율적으로 render()가 호출되는 것을 방지하기 위함
-     */
+  /**
+   * 화면을 갱신하는 render() 함수호출을 최소화하기 위한 기능을 함.
+   * 화면을 그리는 로직은 render() 에서 구현하지만,
+   * 화면을 갱신하기 위해서는 invalidate() 를 호출하라.
+   */
+  public invalidate() {
+    // throttle 로직으로 호출을 최소화
+    // 빈번히 반복되는 invalidate()에 대해 비효율적으로 render()가 호출되는 것을 방지하기 위함
     this.throttle_render();
   }
 }
