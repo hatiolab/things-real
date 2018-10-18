c797e2c1a52746c2eeb4b216c21272b2d62498bf
Hearty Oh <heartyoh@hatiolab.com>
2018-10-19 04:26:07 +0800

    fix version property : sceneModelVersion => version

--- src/migrator/scene-model-migrator.ts
+++ src/migrator/scene-model-migrator.ts
@@ -3,6 +3,9 @@ import SceneModelMigrator1 from './scene-model-migrator-1'
 
 export default class SceneModelMigrator {
   static migrate(model: SceneModel): SceneModel {
+    if (!model)
+      return
+
     switch (model.version || 1) {
       case 1:
         return SceneModelMigrator1.migrate(model)
