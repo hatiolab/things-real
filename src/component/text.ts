72670c16d2e8a0d66615af8821c26bdf28574e96
Hearty Oh <heartyoh@hatiolab.com>
2019-01-04 19:09:30 +0900

    0.1.55

--- src/component/text.ts
+++ src/component/text.ts
@@ -2,23 +2,27 @@
  * Copyright © HatioLab Inc. All rights reserved.
  */
 
-import Component from './component'
-import RealObjectText from './threed/real-object-text'
+import Component from "./component";
+import RealObjectText from "./threed/real-object-text";
 
 export default class Text extends Component {
-
   static get type() {
-    return 'text'
+    return "text";
   }
 
   buildObject3D() {
-    return new RealObjectText(this)
+    return new RealObjectText(this);
   }
 
   get hasTextProperty() {
-    return true
+    return true;
+  }
+
+  onchange(after, before) {
+    /* 모든 변화에 대해서 text substitute 가 동작하도록 object3D를 update함 */
+    super.onchange(after, before);
+    this.object3D.update();
   }
 }
 
-Component.register(Text.type, Text)
-
+Component.register(Text.type, Text);
