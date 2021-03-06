ca360be90035dc0f4578e737adc7a8c49c79b6db
Hearty, Oh <heartyoh@hatiolab.com>
2021-03-06 12:45:34 +0900

    make use puppeteer

--- karma.unit.js
+++ karma.unit.js
@@ -1,28 +1,29 @@
-var webpack = require('./webpack.config')(null, {
-  mode: 'development'
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
+var webpack = require("./webpack.config")(null, {
+  mode: "development",
 });
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'sinon'],
+    basePath: "",
+    frameworks: ["mocha", "chai", "sinon"],
     files: [
-      'node_modules/babel-polyfill/dist/polyfill.js',
-      'test/unit/**/*.ts'
+      "node_modules/babel-polyfill/dist/polyfill.js",
+      "test/unit/**/*.ts",
     ],
-    exclude: [
-    ],
+    exclude: [],
     preprocessors: {
-      'test/**/*.ts': ['webpack']
+      "test/**/*.ts": ["webpack"],
     },
     webpack,
-    reporters: ['progress'],
+    reporters: ["progress"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ["ChromeHeadless"],
     singleRun: false,
-    concurrency: Infinity
-  })
-}
+    concurrency: Infinity,
+  });
+};
